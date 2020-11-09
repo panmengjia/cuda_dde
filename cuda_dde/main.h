68366616f399af6e04886e505966ced8a1181ae8
panmengjia <2926314245@qq.com>
2020-11-09 21:42:51 +0800

    转入cudafft分支，移放代码，代码没有任何警告，可参考

---
+++ cuda_dde/main.h
@@ -0,0 +1,21 @@
+#pragma once
+
+
+#ifndef MAIN_H__
+#define MAIN_H__
+
+
+#include <opencv.hpp>
+#include <iostream>
+//#include <unistd.h>
+#include <fstream>
+
+using namespace cv;
+using namespace std;
+
+
+extern int maindde();
+
+
+
+#endif
