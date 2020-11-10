f826c5e3ef7d081c7ede9a212c74b657c88add08
panmengjia <2926314245@qq.com>
2020-11-11 01:26:28 +0800

    first commit opencvtest

---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include "main.h"
+
+
+int main(int argc, const char ** argv)
+{
+    opencvdft(argc, argv);
+
+    waitKey();
+    return 0;
+}
---
+++ main.h
@@ -0,0 +1,22 @@
+#ifndef MAIN_H__
+#define MAIN_H__
+
+#include <opencv2/opencv.hpp>
+#include <stdio.h>
+#include <iostream>
+
+#include <string>
+#include <stdio.h>
+#include <cuda.h>
+#include "cuda_runtime.h"
+#include <cufft.h>  //要添加cufft.lib cufftw.lib库文件，否则报错无法解析cudafft错误
+
+using namespace cv;
+using namespace std;
+
+
+extern int opencvdft(int argc, const char ** argv);
+
+
+
+#endif
