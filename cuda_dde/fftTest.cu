e284a5508b8dfc83a42a18355a297485f4b669ee
panmengjia <2926314245@qq.com>
2020-11-10 11:49:15 +0800

    正逆变换都可以用一维

--- cuda_dde/fftTest.cu
+++ cuda_dde/fftTest.cu
@@ -1,11 +1,12 @@
 #include "main.h"
 
+//c2c c2r r2c https://blog.csdn.net/qq_17239003/article/details/79090803
 
 void fft1d()
 {
-	const int signalLength = 10;
+	const int signalLength = 15;
 	cufftComplex* signal = (cufftComplex*)malloc(sizeof(cufftComplex) * signalLength);
-	for (int i = 0; i < 10; i++)
+	for (int i = 0; i < signalLength; i++)
 	{
 		(signal+i)->x = (float)(i + 1);
 		(signal+i)->y = 0;
@@ -20,16 +21,39 @@ void fft1d()
 	cudaDeviceSynchronize();//wait to be done
 	cudaMemcpy(signal, signalDev, signalLength * sizeof(cufftComplex), cudaMemcpyDeviceToHost);
 
-	for (int i = 0; i < 10; i++)
+	for (int i = 0; i < signalLength; i++)
 	{
 		cout << (signal+i)->x << " " << (signal+i)->y << endl;
+	}
+
+	//逆变换
+	cufftComplex* signalInverse = (cufftComplex*)malloc(sizeof(cufftComplex) * signalLength);
+	cufftComplex* signalInvDev;
+	cudaMalloc((void**)&signalInvDev, sizeof(cufftComplex) * signalLength);
+	for (int i = 0; i < signalLength; i++)  //全部初始化为0，否则y in-place运算会有错吴数据，也可以不考虑y中的数据
+	{
+		(signal + i)->x = 0;
+		(signal + i)->y = 0;
+	}
+	for (int i = 0; i < signalLength; i++)
+	{
+		cout << (signalInverse + i)->x << " " << (signalInverse + i)->y << endl;
+	}
+
+
+	cufftHandle planInv;
+	cufftPlan1d(&plan, signalLength, CUFFT_C2C, 1);
+	cufftExecC2C(plan, (cufftComplex*)signalDev, signalInvDev, CUFFT_INVERSE);
+	cudaDeviceSynchronize();
+	cudaMemcpy(signalInverse, signalInvDev, signalLength * sizeof(cufftComplex), cudaMemcpyDeviceToHost);
+
+	for (int i = 0; i < signalLength; i++)
+	{
+		cout << (signalInverse + i)->x << " " << (signalInverse + i)->y << endl;
 	}
 
 	cufftDestroy(plan);
 	free(signal);
 	cudaFree(signalDev);
-
-
-
 
 }
