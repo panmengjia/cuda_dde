935b7c2a4800afd4992ee711ce71bdb466be9724
panmengjia <2926314245@qq.com>
2020-11-10 13:25:36 +0800

    提交 之前基础上使用核函数归一化

--- cuda_dde/fftTest.cu
+++ cuda_dde/fftTest.cu
@@ -1,9 +1,27 @@
 #include "main.h"
 
 //c2c c2r r2c https://blog.csdn.net/qq_17239003/article/details/79090803
+//很好的博客文章：https://zhuanlan.zhihu.com/p/34587739
+
+
+__global__ void kernel_normalize1d(cufftComplex* signal,const int signalLength)
+{
+	int idx = blockIdx.x * blockDim.x + threadIdx.x;
+
+	if (idx < signalLength) //是避免访问非法内存吗
+	{
+		(signal + idx)->x /= signalLength;
+		(signal + idx)->y /= signalLength;
+	}
+}
 
 void fft1d()
 {
+	// Use cuda event to catch time
+	cudaEvent_t start, stop;
+	cudaEventCreate(&start);
+	cudaEventCreate(&stop);
+
 	const int signalLength = 15;
 	cufftComplex* signal = (cufftComplex*)malloc(sizeof(cufftComplex) * signalLength);
 	for (int i = 0; i < signalLength; i++)
@@ -46,6 +64,23 @@ void fft1d()
 	cufftExecC2C(plan, (cufftComplex*)signalDev, signalInvDev, CUFFT_INVERSE);
 	cudaDeviceSynchronize();
 	cudaMemcpy(signalInverse, signalInvDev, signalLength * sizeof(cufftComplex), cudaMemcpyDeviceToHost);
+	for (int i = 0; i < signalLength; i++)
+	{
+		cout << (signalInverse + i)->x << " " << (signalInverse + i)->y << endl;
+	}
+	//核归一化函数
+	//可以测试下托管方式
+	const unsigned int blockX = 32;
+	// Specify block size
+	const dim3 block(blockX, 1);
+	const dim3 grid(1);
+
+
+	// Start time
+	cudaEventRecord(start, 0);  //暂时不用
+	kernel_normalize1d <<< grid, block >>> (signalInvDev,signalLength);//可能实际启动的线程数是32
+	cudaMemcpy(signalInverse, signalInvDev, signalLength * sizeof(cufftComplex),cudaMemcpyDeviceToHost);
+
 
 	for (int i = 0; i < signalLength; i++)
 	{
@@ -55,5 +90,11 @@ void fft1d()
 	cufftDestroy(plan);
 	free(signal);
 	cudaFree(signalDev);
+
+}
+
+void fft2d()
+{
+
 
 }
